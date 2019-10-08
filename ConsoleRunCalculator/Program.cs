de931c3be5fd844eedbd37b137aa36ba4f22c1ef
TC-NCC-1701-D <tahmidc@hotmail.com>
2019-10-08 14:36:54 -0700

    Add project files.

---
+++ ConsoleRunCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleRunCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
